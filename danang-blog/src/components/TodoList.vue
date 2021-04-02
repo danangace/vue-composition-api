d96c0186e30e3cb85e1028e5de78b346c64cf9ae
danangace <danangbahari89@gmail.com>
2021-04-02 13:10:59 +0700

    first project todo

---
+++ danang-blog/src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="todo-list">
+    <ul>
+      <li v-for="(data,index) in todos" :key="index">
+        {{data}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+    todos: Array
+  },
+  setup (props) {
+    console.log(props.todos)
+    // props.todos.forEach(todo => console.log(todo))
+    return {
+      todos: props.todos
+    }
+  }
+}
+</script>
+
+<style scoped>
+p{
+  margin-bottom: 0px;
+}
+</style>
---
+++ danang-blog/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <h1>My Todo List</h1>
+    <todo-list :todos="todos"></todo-list>
+    <form>
+      <input type="text" placeholder="Masukkan todo" name="todo-input" v-model="todo">
+      <button @click.prevent="handleSubmitTodo">Tambah Todo</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import TodoList from '../components/TodoList.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    TodoList
+  },
+  setup () {
+    const todo = ref('')
+    const todos = ref([])
+
+    const handleSubmitTodo = () => {
+      todos.value.push(todo.value)
+      todo.value = ''
+    }
+
+    return {
+      todo,
+      todos,
+      handleSubmitTodo
+    }
+  }
+}
+</script>
